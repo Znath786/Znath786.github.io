7f428023119de24d04f5873dd7840ee92d79010b
Znath786 <49847790+Znath786@users.noreply.github.com>
2020-03-20 22:49:25 -0400

    Files for number animation lab

---
+++ Numbers2.js
@@ -0,0 +1,251 @@
+numbers={
+	src : "numSprites2.png",
+	img: undefined,
+	frame:0,
+	offset: 0,
+	"frames": [
+		{
+			"filename": "tile000.png",
+			"frame": {
+				"x": 1,
+				"y": 1,
+				"w": 314,
+				"h": 729
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 314,
+				"h": 729
+			},
+			"sourceSize": {
+				"w": 314,
+				"h": 729
+			}
+		},
+		{
+			"filename": "tile001.png",
+			"frame": {
+				"x": 317,
+				"y": 1,
+				"w": 302,
+				"h": 714
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 302,
+				"h": 714
+			},
+			"sourceSize": {
+				"w": 302,
+				"h": 714
+			}
+		},
+		{
+			"filename": "tile002.png",
+			"frame": {
+				"x": 621,
+				"y": 1,
+				"w": 327,
+				"h": 729
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 327,
+				"h": 729
+			},
+			"sourceSize": {
+				"w": 327,
+				"h": 729
+			}
+		},
+		{
+			"filename": "tile003.png",
+			"frame": {
+				"x": 950,
+				"y": 1,
+				"w": 312,
+				"h": 729
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 312,
+				"h": 729
+			},
+			"sourceSize": {
+				"w": 312,
+				"h": 729
+			}
+		},
+		{
+			"filename": "tile004.png",
+			"frame": {
+				"x": 1,
+				"y": 732,
+				"w": 327,
+				"h": 729
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 327,
+				"h": 729
+			},
+			"sourceSize": {
+				"w": 327,
+				"h": 729
+			}
+		},
+		{
+			"filename": "tile005.png",
+			"frame": {
+				"x": 330,
+				"y": 732,
+				"w": 314,
+				"h": 729
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 314,
+				"h": 729
+			},
+			"sourceSize": {
+				"w": 314,
+				"h": 729
+			}
+		},
+		{
+			"filename": "tile006.png",
+			"frame": {
+				"x": 646,
+				"y": 732,
+				"w": 297,
+				"h": 729
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 297,
+				"h": 729
+			},
+			"sourceSize": {
+				"w": 297,
+				"h": 729
+			}
+		},
+		{
+			"filename": "tile007.png",
+			"frame": {
+				"x": 945,
+				"y": 732,
+				"w": 325,
+				"h": 729
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 325,
+				"h": 729
+			},
+			"sourceSize": {
+				"w": 325,
+				"h": 729
+			}
+		},
+		{
+			"filename": "tile008.png",
+			"frame": {
+				"x": 1264,
+				"y": 1,
+				"w": 322,
+				"h": 729
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 322,
+				"h": 729
+			},
+			"sourceSize": {
+				"w": 322,
+				"h": 729
+			}
+		},
+		{
+			"filename": "tile009.png",
+			"frame": {
+				"x": 1272,
+				"y": 732,
+				"w": 300,
+				"h": 729
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 300,
+				"h": 729
+			},
+			"sourceSize": {
+				"w": 300,
+				"h": 729
+			}
+		}
+		/*{
+			"filename": "tile000.png",
+			"frame": {
+				"x": 1272,
+				"y": 732,
+				"w": 300,
+				"h": 729
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 300,
+				"h": 729
+			},
+			"sourceSize": {
+				"w": 300,
+				"h": 729
+			}
+		}  */
+
+	],
+	"meta": {
+		"app": "http://www.codeandweb.com/texturepacker",
+		"version": "1.0",
+		"image": "spritesheet.png",
+		"format": "RGBA8888",
+		"size": {
+			"w": 1587,
+			"h": 1462
+		},
+		"scale": "1"
+	}
+}
---
+++ animation3.js
@@ -0,0 +1,77 @@
+"use strict";
+
+var Scene = {
+    canvas : undefined,
+    canvasContext : undefined,
+	sprite: undefined
+};
+
+Scene.start = function () {
+	// Get the canvas and it's context.
+    Scene.canvas = document.getElementById("myCanvas");
+    Scene.canvasContext = Scene.canvas.getContext("2d");
+    Scene.canvasContext.fillStyle = "purple";
+    Scene.canvasContext.fillRect(0,0, Scene.canvas.width, Scene.canvas.height);
+	
+	// Seup the numbers to be displayed.
+    Scene.sprite = numbers;
+	
+	// Attach the image to be used for the sprite.
+    Scene.sprite.img = new Image();
+    Scene.sprite.img.src = Scene.sprite.src;
+	
+	// Wait till the number image is loaded before starting the animation.
+	Scene.sprite.img.onload = function() {	  	
+        Scene.sprite.offset=(Scene.canvas.width)/2-0.5*(Scene.sprite.frames[Scene.sprite.frame].frame.w) ;
+        Scene.canvasContext.drawImage(Scene.sprite.img,Scene.sprite.frames[0].frame.x,Scene.sprite.frames[0].frame.y,Scene.sprite.frames[0].frame.w,Scene.sprite.frames[0].frame.h,Scene.sprite.offset,0,Scene.sprite.frames[0].frame.w,Scene.sprite.frames[0].frame.h);
+        Scene.mainloop();
+    }
+};
+
+// Once the basic HTML document is loaded and its parsing has taken place, start the scene.
+document.addEventListener( 'DOMContentLoaded', Scene.start);
+
+Scene.clearCanvas = function () {
+    Scene.canvasContext.globalCompositeOperation = 'destination-over' // taken from Stack Overflow
+    Scene.canvasContext.fillStyle = "purple";
+    Scene.canvasContext.fillRect(0, 0, Scene.canvas.width, Scene.canvas.height);
+};
+
+function mainLoop() {
+    Scene.clearCanvas();
+    Scene.update();
+    Scene.draw();
+	
+	// Animate at 2 frames a second.
+    window.setTimeout(mainLoop, 500);
+};
+
+Scene.update = function () {
+	// Set the canvas width to be that of the display Window. Which helps if you resize the window.
+  	Scene.canvas.width = Scene.canvas.width;
+	
+    // Set the location of the next frame. 
+  	Scene.sprite.offset=(Scene.canvas.width)/2-0.5*(Scene.sprite.frames[Scene.sprite.frame].frame.w) ;
+};
+var a=-1;
+Scene.draw = function () {
+    a++;
+    Scene.canvasContext.fillStyle = "purple";
+    Scene.canvasContext.fillRect(0, 0, Scene.canvas.width, Scene.canvas.height);
+
+    if(a==0){
+        Scene.canvasContext.drawImage(Scene.sprite.img,Scene.sprite.frames[0].frame.x,Scene.sprite.frames[0].frame.y,Scene.sprite.frames[0].frame.w,Scene.sprite.frames[0].frame.h,Scene.sprite.offset,0,Scene.sprite.frames[0].frame.w,Scene.sprite.frames[0].frame.h);
+    }
+    else if(a<10){
+        Scene.sprite.frame++; 
+        Scene.canvasContext.drawImage(Scene.sprite.img,Scene.sprite.frames[Scene.sprite.frame].frame.x,Scene.sprite.frames[Scene.sprite.frame].frame.y,Scene.sprite.frames[Scene.sprite.frame].frame.w,Scene.sprite.frames[Scene.sprite.frame].frame.h,Scene.sprite.offset,0,Scene.sprite.frames[Scene.sprite.frame].frame.w,Scene.sprite.frames[Scene.sprite.frame].frame.h);
+    }
+    else{
+        Scene.sprite.offset=Scene.canvas.width/2 - Scene.sprite.frames[1].frame.w;
+        Scene.canvasContext.drawImage(Scene.sprite.img,Scene.sprite.frames[1].frame.x,Scene.sprite.frames[1].frame.y,Scene.sprite.frames[1].frame.w,Scene.sprite.frames[1].frame.h,Scene.sprite.offset,0,Scene.sprite.frames[1].frame.w,Scene.sprite.frames[1].frame.h);
+        Scene.sprite.offset= (Scene.canvas.width/2);
+        Scene.canvasContext.drawImage(Scene.sprite.img,Scene.sprite.frames[0].frame.x,Scene.sprite.frames[0].frame.y,Scene.sprite.frames[0].frame.w,Scene.sprite.frames[0].frame.h,Scene.sprite.offset,0,Scene.sprite.frames[0].frame.w,Scene.sprite.frames[0].frame.h);
+    
+    }     
+};
+
